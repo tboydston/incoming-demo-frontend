7cae50e9f94a57a20664fdc4d86f4108edb475fa
Tracey Boydston <tracey@optimalprime.com>
2023-02-10 12:10:19 +0800

    Add styles and update js.

--- src/js/app.js
+++ src/js/app.js
@@ -1,53 +1,151 @@
+const dataURL = "https://incoming-demo.optimalprime.com/data/depositData.json";
+const refreshPeriod = 20000;
+
 async function loadData() {
-  let depositData = {};
-  let addressData = {};
-
   try {
-    const depositDataResponse = await fetch("../data/depositData.json");
-    depositData = await depositDataResponse.json();
-    const addressDataResponse = await fetch("../data/addressData.json");
-    addressData = await depositDataResponse.json();
+    const depositDataResponse = await fetch(dataURL);
+
+    let depositData = await depositDataResponse.json();
+
+    updateChainData(depositData, "BTC");
+    document.getElementById(
+      `lastDataRefresh`
+    ).innerHTML = `Data refreshes every ${
+      refreshPeriod / 1000
+    } seconds. <br> Last Refresh: ${new Date()}`;
   } catch (error) {
-    console.error("Error:", error.message);
+    console.error("Error:", error);
   }
 
-  loadTable(depositData, "BTC");
-  loadTable(depositData, "BTCt");
+  setTimeout(loadData, refreshPeriod);
 }
 
-function loadTable(depositData, coin) {
-  var table = parseToHTMLTable(depositData[coin].deposits);
-  document.getElementById(`${coin}-Deposits`).appendChild(table);
+function updateChainData(depositData, coin) {
+  document.getElementById(`${coin}-address-table`).innerHTML = "";
+  document.getElementById(`${coin}-deposit-table`).innerHTML = "";
+
+  const coinData = depositData[coin];
+
+  let addressTable = createAddressesTable(depositData[coin].addresses, "btc");
+  let depositTable = createDepositsTable(depositData[coin], "btc");
+
+  document.getElementById(`${coin}-chainHeight`).innerHTML =
+    coinData.chainHeight;
+  document.getElementById(`${coin}-highestDepositBlock`).innerHTML =
+    coinData.highestDepositBlock;
+  document.getElementById(`${coin}-lastBlockTime`).innerHTML = new Date(
+    coinData.lastBlockTime
+  );
+  document.getElementById(`${coin}-lastDepositTime`).innerHTML = new Date(
+    coinData.lastDepositTime
+  );
+
+  document.getElementById(`${coin}-address-table`).appendChild(addressTable);
+  document.getElementById(`${coin}-deposit-table`).appendChild(depositTable);
 }
 
-// Function to parse JSON data to HTML table
-function parseToHTMLTable(data) {
-  var table = document.createElement("table");
-  var thead = document.createElement("thead");
-  var tbody = document.createElement("tbody");
-  var headRow = document.createElement("tr");
+function createAddressesTable(data, chainName) {
+  let table = document.createElement("table");
+  let tbody = document.createElement("tbody");
 
-  // Extract the keys from the first object in the data array
-  // These will be used as the table headers
-  var headers = Object.keys(data[0]);
-  for (var i = 0; i < headers.length; i++) {
-    var th = document.createElement("th");
-    th.innerHTML = headers[i];
-    headRow.appendChild(th);
-  }
-  thead.appendChild(headRow);
-  table.appendChild(thead);
+  table.classList.add("table");
+  table.classList.add("table-striped");
 
-  // Loop through the data array to create the rows and cells
-  for (var i = 0; i < data.length; i++) {
-    var row = document.createElement("tr");
-    for (var j = 0; j < headers.length; j++) {
-      var cell = document.createElement("td");
-      cell.innerHTML = data[i][headers[j]];
-      row.appendChild(cell);
-    }
+  let headerRow = document.createElement("tr");
+  let pathHeader = document.createElement("th");
+
+  pathHeader.textContent = "Path";
+  headerRow.appendChild(pathHeader);
+  let addressHeader = document.createElement("th");
+  addressHeader.textContent = "Address";
+  headerRow.appendChild(addressHeader);
+  table.appendChild(headerRow);
+
+  for (let item of data) {
+    let addLink = `<a href="https://www.blockchain.com/explorer/addresses/${chainName}/${item.address}" target="_blank" >${item.address}</a>`;
+
+    let row = document.createElement("tr");
+    let pathCell = document.createElement("td");
+    pathCell.textContent = item.path;
+    row.appendChild(pathCell);
+    let addressCell = document.createElement("td");
+    addressCell.innerHTML = addLink;
+    row.appendChild(addressCell);
     tbody.appendChild(row);
   }
+
+  table.appendChild(tbody);
+  return table;
+}
+
+function createDepositsTable(chainData, chainName) {
+  let data = chainData.deposits;
+
+  let table = document.createElement("table");
+  let tbody = document.createElement("tbody");
+
+  table.classList.add("table");
+  table.classList.add("table-striped");
+
+  let headerRow = document.createElement("tr");
+
+  let blockHeader = document.createElement("th");
+  blockHeader.textContent = "Block";
+
+  let confirmationsHeader = document.createElement("th");
+  confirmationsHeader.textContent = "Confirmations";
+
+  let txIdHeader = document.createElement("th");
+  txIdHeader.textContent = "Txid";
+
+  let addressHeader = document.createElement("th");
+  addressHeader.textContent = "Address";
+
+  let amountHeader = document.createElement("th");
+  amountHeader.textContent = "Amount";
+
+  headerRow.appendChild(blockHeader);
+  headerRow.appendChild(confirmationsHeader);
+  headerRow.appendChild(txIdHeader);
+  headerRow.appendChild(addressHeader);
+  headerRow.appendChild(amountHeader);
+
+  table.appendChild(headerRow);
+
+  for (let item of data) {
+    let blockLink = `<a href="https://www.blockchain.com/explorer/blocks/${chainName}/${item.block}" target="_blank" >${item.block}</a>`;
+    let txLink = `<a href="https://www.blockchain.com/explorer/transactions/${chainName}/${
+      item.txid
+    }" target="_blank" >${item.txid.slice(0, 15)}...</a>`;
+    let addLink = `<a href="https://www.blockchain.com/explorer/addresses/${chainName}/${item.address}" target="_blank" >${item.address}</a>`;
+    let confirmations =
+      item.block === 0 ? "Unconfirmed" : chainData.chainHeight + 1 - item.block;
+
+    let row = document.createElement("tr");
+
+    let blockCell = document.createElement("td");
+    blockCell.innerHTML = blockLink;
+    row.appendChild(blockCell);
+
+    let confirmationsCell = document.createElement("td");
+    confirmationsCell.textContent = confirmations;
+    row.appendChild(confirmationsCell);
+
+    let txIdCell = document.createElement("td");
+    txIdCell.innerHTML = txLink;
+    row.appendChild(txIdCell);
+
+    let addressCell = document.createElement("td");
+    addressCell.innerHTML = addLink;
+    row.appendChild(addressCell);
+
+    let amountCell = document.createElement("td");
+    amountCell.textContent = item.amount;
+    row.appendChild(amountCell);
+
+    tbody.appendChild(row);
+  }
+
   table.appendChild(tbody);
   return table;
 }
